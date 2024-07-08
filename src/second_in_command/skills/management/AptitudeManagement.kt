aceb0922e9a324a0282c30eb886987e9e93e0a9b
Lukas22041 <lukas.kurth04@gmail.com>
2024-07-09 01:56:59 +0200

    management tree placeholders

---
+++ src/second_in_command/skills/management/AptitudeManagement.kt
@@ -0,0 +1,45 @@
+package second_in_command.skills.management
+
+import com.fs.starfarer.api.campaign.AICoreOfficerPlugin
+import com.fs.starfarer.api.campaign.FleetDataAPI
+import com.fs.starfarer.api.characters.MutableCharacterStatsAPI
+import com.fs.starfarer.api.impl.campaign.skills.BaseSkillEffectDescription
+import com.fs.starfarer.api.impl.hullmods.Automated
+import com.fs.starfarer.api.util.Misc
+import second_in_command.specs.SCAptitudeSection
+import second_in_command.specs.SCBaseAptitudePlugin
+
+class AptitudeManagement : SCBaseAptitudePlugin() {
+
+    companion object {
+
+    }
+
+    override fun getOriginSkillId(): String {
+        return "sc_management_crew_training"
+    }
+
+    override fun createSections() {
+
+        var section1 = SCAptitudeSection(true, 0, "leadership1")
+        section1.addSkill("sc_management_top_condition")
+        section1.addSkill("sc_management_well_organized")
+        section1.addSkill("sc_management_authority")
+        section1.addSkill("sc_management_re_entry")
+        section1.addSkill("sc_management_command_and_conquer")
+        addSection(section1)
+
+        var section2 = SCAptitudeSection(false, 2, "leadership2")
+        section2.addSkill("sc_management_officer_management")
+        section2.addSkill("sc_management_officer_training")
+        addSection(section2)
+
+        var section3 = SCAptitudeSection(false, 4, "leadership5")
+        section3.addSkill("sc_management_best_of_the_best")
+        section3.addSkill("sc_management_in_good_hands")
+        addSection(section3)
+
+    }
+
+
+}
--- src/second_in_command/skills/management/BestOfTheBest.kt
+++ src/second_in_command/skills/management/BestOfTheBest.kt
@@ -1,4 +1,4 @@
-package second_in_command.skills.automated
+package second_in_command.skills.management
 
 import com.fs.starfarer.api.combat.MutableShipStatsAPI
 import com.fs.starfarer.api.combat.ShipAPI
@@ -7,33 +7,22 @@ import com.fs.starfarer.api.ui.TooltipMakerAPI
 import com.fs.starfarer.api.util.Misc
 import second_in_command.specs.SCBaseSkillPlugin
 
-class AutonomousExpertise : SCBaseSkillPlugin() {
-
-    companion object {
-        var points = 60
-    }
+class BestOfTheBest : SCBaseSkillPlugin() {
 
     override fun getAffectsString(): String {
-        return "all automated ships"
+        return "all ships in the fleet"
     }
 
     override fun addTooltip(tooltip: TooltipMakerAPI) {
 
-        tooltip.addPara("+$points additional automated points ", 0f, Misc.getHighlightColor(), Misc.getHighlightColor())
-        tooltip.addPara("   - Additional automated points enable larger autonomous fleets ", 0f, Misc.getTextColor(), Misc.getHighlightColor())
-
     }
 
     override fun applyEffectsBeforeShipCreation(stats: MutableShipStatsAPI?, variant: ShipVariantAPI, hullSize: ShipAPI.HullSize?, id: String?) {
-        if (Misc.isAutomated(stats)) {
 
-        }
     }
 
     override fun applyEffectsAfterShipCreation(ship: ShipAPI?, variant: ShipVariantAPI, id: String?) {
-        if (Misc.isAutomated(ship)) {
 
-        }
     }
 
 
