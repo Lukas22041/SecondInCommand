244e039e58a024bbe9417242897ce7c894ffb625
Lukas22041 <lukas.kurth04@gmail.com>
2024-08-10 14:14:23 +0200

    Aptitude doc

--- src/second_in_command/specs/SCBaseAptitudePlugin.java
+++ src/second_in_command/specs/SCBaseAptitudePlugin.java
@@ -9,47 +9,53 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**Instantiated whenever needed, do not save class-scope variables within */
 public abstract class SCBaseAptitudePlugin {
 
     public SCAptitudeSpec spec;
-    private final List<SCAptitudeSection> sections = new ArrayList<>();
+
+
+
+
+
 
     public final void addSection(SCAptitudeSection section) {
         sections.add(section);
     }
 
-    public final List<SCAptitudeSection> getSections() {
-        return sections;
-    }
-
-    public final void clearSections() {
-        sections.clear();
-    }
-
+    /**Required, this is the first skill in the aptitude that will always be unlocked by default. */
     public abstract String getOriginSkillId();
 
-    public final SCSkillSpec getOriginSkillSpec() {
-        return SCSpecStore.getSkillSpec(getOriginSkillId());
-    }
-
-    public final SCBaseSkillPlugin getOriginSkillPlugin() {
-        return getOriginSkillSpec().getPlugin();
-    }
-
+    /**Required, used to decide which skills are added, and how they are organised within. */
     public abstract void createSections();
 
+
+
+    /**
+     * Change the weight of an aptitude being picked for the fleet generation.
+     * The average value should be 1, set it lower value for it to be rarer, higher for it to be common
+     * If your aptitude is very specific to some specific use-case, for example dmods, change the returned value based on those conditions.
+     *
+     * @return weight, lower is rarer, 0 is never*/
     public abstract Float getNPCFleetSpawnWeight(SCData data, CampaignFleetAPI fleet);
 
+    /**
+     * Chance for this aptitude to be picker over others on comm-links.
+     * @return weight, lower is rarer, 0 is never */
     public Float getMarketSpawnweight(MarketAPI market) {
         return spec.getSpawnWeight();
     }
 
+    /**
+     * Chance for the aptitude to be picked over others for cryo-pods.
+     * @return weight, lower is rarer, 0 is never */
     public Float getCryopodSpawnWeight(StarSystemAPI System) {
         return spec.getSpawnWeight();
     }
 
 
-    /**Only used for NPC fleet generation. Guarantees its a priority pick when the conditon is set, should be used sparingly. */
+    /**Only used for NPC fleet generation. Guarantees its picked when returned true, should be used sparingly.
+     * This is mostly used in case the aptitude is incompatible with another, and this one should have priority over the other*/
     public Boolean guaranteePick(CampaignFleetAPI fleet) {
         return false;
     }
@@ -72,5 +78,32 @@ public abstract class SCBaseAptitudePlugin {
 
     public List<String> getTags() {
         return spec.getTags();
+    }
+
+
+
+
+
+    //Internal Use Only
+    public final SCSkillSpec getOriginSkillSpec() {
+        return SCSpecStore.getSkillSpec(getOriginSkillId());
+    }
+
+    //Internal Use Only
+    public final SCBaseSkillPlugin getOriginSkillPlugin() {
+        return getOriginSkillSpec().getPlugin();
+    }
+
+    //Internal Use Only
+    private final List<SCAptitudeSection> sections = new ArrayList<>();
+
+    //Internal Use Only
+    public final List<SCAptitudeSection> getSections() {
+        return sections;
+    }
+
+    //Internal Use Only
+    public final void clearSections() {
+        sections.clear();
     }
 }
